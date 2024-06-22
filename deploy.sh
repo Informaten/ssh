764d22dd934cc7b7bcbbfb7a971853dbb42e1d3e
Luxaaa <35200689+Luxaaa@users.noreply.github.com>
2024-06-22 23:02:44 +0200

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+REPO_URL="https://raw.githubusercontent.com/informaten/ssh/main/keys/"
+
+KEYS=(
+  "luca.pub"
+  "max.pub"
+  "alec.pub"
+)
+
+LOCAL_USER=$(whoami)
+USER_HOME=$(getent passwd "$LOCAL_USER" | cut -d: -f6)
+AUTHORIZED_KEYS="${USER_HOME}/.ssh/authorized_keys"
+mkdir -p "${USER_HOME}/.ssh"
+
+
+for KEY in "${KEYS[@]}"; do
+  KEY_URL="${REPO_URL}${KEY}"
+  echo "Adding key from ${KEY_URL}"
+  PUBLIC_KEY=$(curl -s "${KEY_URL}")
+
+  if grep -qF "${PUBLIC_KEY}" "${AUTHORIZED_KEYS}"; then
+    echo "Key already exists"
+    exit 0
+  fi
+
+  chown" ${LOCAL_USER}":"${LOCAL_USER}" "${AUTHORIZED_KEYS}"
+  chmod 600 "${AUTHORIZED_KEYS}"
+
+  echo "Key added to ${AUTHORIZED_KEYS}"
+
+done
+
