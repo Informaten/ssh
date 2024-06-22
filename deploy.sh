062fbfb5183e48086ffc654f0cfefc17de1f2b0c
Luxaaa <35200689+Luxaaa@users.noreply.github.com>
2024-06-22 23:20:18 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -13,20 +13,22 @@ USER_HOME=$(getent passwd "$LOCAL_USER" | cut -d: -f6)
 AUTHORIZED_KEYS="${USER_HOME}/.ssh/authorized_keys"
 mkdir -p "${USER_HOME}/.ssh"
 touch "${AUTHORIZED_KEYS}"
+chown "${LOCAL_USER}":"${LOCAL_USER}" "${AUTHORIZED_KEYS}"
+chmod 600 "${AUTHORIZED_KEYS}"
 
 
 for KEY in "${KEYS[@]}"; do
   KEY_URL="${REPO_URL}${KEY}"
   echo "Adding key from ${KEY_URL}"
   PUBLIC_KEY=$(curl -s "${KEY_URL}")
-
+  
   if grep -qF "${PUBLIC_KEY}" "${AUTHORIZED_KEYS}"; then
     echo "Key already exists"
     exit 0
   fi
 
-  chown "${LOCAL_USER}":"${LOCAL_USER}" "${AUTHORIZED_KEYS}"
-  chmod 600 "${AUTHORIZED_KEYS}"
+  echo "${PUBLIC_KEY}" >> "${AUTHORIZED_KEYS}"
+
 
   echo "Key added to ${AUTHORIZED_KEYS}"
 
